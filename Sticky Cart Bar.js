07dd7cde11db24f9baf38de26ab5c1c25276f914
abdouaet <haithemboukhari9@gmail.com>
2024-11-15 21:34:40 +0100

    Add files via upload

---
+++ Sticky Cart Bar.js
@@ -0,0 +1,38 @@
+// Show & Hide the sticky cart bar
+document.addEventListener('DOMContentLoaded', function() {
+	const stickyCartBar = document.querySelector('.sticky-cart-bar');
+	const expressCheckoutSection = document.getElementById('express-checkout-section');
+
+	if (!stickyCartBar || !expressCheckoutSection) return;
+
+	// Initially hide the sticky cart bar
+	stickyCartBar.classList.remove('show');
+
+	document.addEventListener('scroll', function() {
+		const rect = expressCheckoutSection.getBoundingClientRect();
+		const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+
+		if (!isVisible) {
+			stickyCartBar.classList.add('show'); // Show the sticky button
+		} else {
+			stickyCartBar.classList.remove('show'); // Hide the sticky button
+		}
+	});
+});
+
+// Select the sticky quantity selector
+const quantities = document.querySelectorAll('.quantity');
+if (quantities.length > 1) {
+	const stickyQuantity = quantities[1]; // Only access if there are at least two elements
+	stickyQuantity.classList = "sticky quantity";
+
+	const button = document.querySelectorAll("button.single-submit");
+	const stickyButton = button[1];
+
+	const anchor = document.createElement("a");
+	anchor.href = "#express-checkout-form";
+	anchor.className = "sticky-anchor single-submit";
+	anchor.innerText = stickyButton.innerText;
+
+	stickyButton.replaceWith(anchor);
+}
